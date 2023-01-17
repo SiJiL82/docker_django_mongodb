32cca4431a37d7886770084d69f7de001518a352
SiJiL82 <sven.lowry@gmail.com>
2023-01-17 19:34:41 +0000

    Add DB entrypoint

---
+++ app/entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+echo "Waiting for database..."
+
+while ! nc -z $DB_HOST $DB_PORT; do
+    sleep 0.1
+done
+
+echo "Database started"
+
+
+python manage.py flush --no-input
+python manage.py migrate
+
+exec "$@"
